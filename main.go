18cd75286e72bcee1fc03ffe27672a9d38116e7f
shanhuijie <2270149665@qq.com>
2019-10-29 17:05:24 +0800

    'Cityinfo-for-ip'

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 import(
-	 "github.com/juju/errors"
-	 "GoWatch/getuuid"
+	"github.com/juju/errors"
+	"GoWatch/getuuid"
 	e "errors"
 	"fmt"
+	"GoWatch/mapapi"
 )
 
 func te() string{
 	var i int
 	var cr string
-	i = i+7
+	i ++
 	if i>3{
 	ter :=	e.New("number out to range")
 	fmt.Println(ter)
@@ -25,25 +26,11 @@ func uuid(){
 }
 
 var snum int
+var ip string
+var Point mapapi.JsPoint
 func main(){
-	uuid()
-	uuid()
-	uuid()
-	//s:=te()
-	// fmt.Println("ess",s)
-	// for i := 0; i<=10;i++{
-	// 	snum = i
-	// 	switch snum {
-	// 	case 1,2,3 :
-	// 		fmt.Println(1)
-		
-	// 	case 4,5,6 :
-	// 		fmt.Println(2)
-		
-	// 	case 7,8,9 :
-	// 		fmt.Println(3)
-	// 	}
-	// }  
+	ip = "125.119.244.70"
 	
-
+	Point = mapapi.Getpoint(ip)
+	fmt.Println(Point)
 }
---
+++ mapapi/mapapi.go
@@ -0,0 +1,54 @@
+package mapapi
+
+import(
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
+var Ip string
+
+type IpJson struct{
+	Address  string			`json:"address"`
+	Content  JsContent 	`json:"content"`
+	Status	 int 			`json:"status"`
+}
+
+type JsContent struct{
+	Address  		string 				`json:"address"`
+	Address_detail  JsAddress_detail 	`json:"address_detail"`
+	Point 			JsPoint 			`json:"point"`
+}
+
+type JsAddress_detail struct{
+	City			string 		`json:"city"`
+	City_code		int 		`json:"city_code"`
+	District		string		`json:"district"`
+	Province		string		`json:"province"`
+	Street			string		`json:"street"`
+	Street_number	string		`json:"street_number"`
+}
+
+type JsPoint struct{
+	X	string 	`json:"x"`
+	Y	string		`json:"y"`
+}
+
+
+func Getpoint(Ip string)(ppoint JsPoint) {
+	
+	url := "http://api.map.baidu.com/location/ip?ip="+Ip+"&ak=vKgTlhoXlrEFFmLGBD4E04Gdv29ZYMxX&coor=bd09ll"
+	resp, err := http.Get(url); if err != nil{
+		fmt.Println(err)
+	}
+	body , errred := ioutil.ReadAll(resp.Body); if errred != nil{
+		fmt.Println(errred)
+	}
+	//fmt.Println(string(body))
+	jsonip := IpJson{}
+	jserr := json.Unmarshal(body,&jsonip); if jserr != nil{
+		fmt.Println("jserr is error::",jserr)
+	}
+	ppoint = jsonip.Content.Point
+	return ppoint
+}
