2301ae7a61d77ac8f435259d9ef37ae93574e5d7
shanhuijie <2270149665@qq.com>
2019-11-19 09:08:09 +0800

    '2019-11-19-chanSend'

--- heartbeat/websocket.go
+++ heartbeat/websocket.go
@@ -46,25 +46,21 @@ var url string
 var i int
 
 func Client(iplist []string,locIp string) {
-	fmt.Println(iplist)
 	chanMax := len(iplist)
-	writeChan := make(chan int, chanMax)
+	writeChan := make(chan string, chanMax)
 	for {
-		i++
-		time.Sleep(1 * time.Second)
-		if i == 999999999 {
-			i = 0
-			i++
+		for _,fsmIp := range iplist{
+			writeChan<-fsmIp
+			go Send(writeChan,locIp)
 		}
-		writeChan <- i
-		go Send(writeChan, iplist,locIp)
-
+		
+		time.Sleep(1 * time.Second)
 	}
 
 }
 
-func Send(writeChan chan int, iplist []string,locIp string) {
-	for _, v := range iplist {
+func Send(writeChan chan string,locIp string) {
+		v := <-writeChan
 		origin = "http://" + strings.TrimSpace(v) + ":80/"
 		url = "ws://" + strings.TrimSpace(v) + ":80/echo"
 		ws, err := websocket.Dial(url, "", origin)
@@ -72,11 +68,9 @@ func Send(writeChan chan int, iplist []string,locIp string) {
 			message := []byte(base64.StdEncoding.EncodeToString([]byte(locIp)))
 			ws.Write(message)
 			fmt.Printf("Client---Send: %s---%v--ip:--%v\n", message,time.Now(),v)
-			<-writeChan
 			var msg = make([]byte, 512)
 			m, _ := ws.Read(msg)
 			fmt.Printf("Client---Receive: %s---%v--ip:--%v\n", msg[:m],time.Now(),v)
 			
 		}
-	}
 }
