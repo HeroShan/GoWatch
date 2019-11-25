5fafaa9cc627fde38f32b14acb56b983bf247cdb
shanhuijie <2270149665@qq.com>
2019-11-25 09:15:52 +0800

    2019-11-25fixdelcookie

--- main.go
+++ main.go
@@ -158,9 +158,10 @@ func fmsgetip(w http.ResponseWriter, r *http.Request){
 
 func monitoring(){
 	for{
+		
 		time.Sleep(24 * time.Hour)
 		wk := time.Now().Weekday().String()
-		if wk == "Sunday"{
+		if wk == "Sunday" || wk == "Wednesday"{
 			createToken.DelExpireToken()
 		}
 	}
