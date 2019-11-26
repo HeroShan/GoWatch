47774329124e26184ebc718eec8d93c460babfe3
shanhuijie <2270149665@qq.com>
2019-11-26 15:13:51 +0800

    '2019-11-26fixbug'

--- heartbeat/websocket.go
+++ heartbeat/websocket.go
@@ -10,75 +10,73 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-//wingman WINGMAN  ↑↑ ↓↓  ←→  ←→ BABA卒
-func EchoHandler(ws *websocket.Conn) {	
-    msg := make([]byte, 512)	
-    n, err := ws.Read(msg)	
-    if err != nil {	
-        log.Fatal(err)	
-	}	
-	clientip,_ := base64.StdEncoding.DecodeString(string(msg[:n]))
-	fmt.Println(string(clientip))
-	
-	
-	Hplist := GetConfIp()
-	for _,v := range Hplist{
-		v = strings.TrimSpace(v)
-		if v == string(clientip){
-			send_msg := "wingman WINGMAN  ↑↑ ↓↓  ←→  ←→ BABA卒"+v	
-			_, err := ws.Write([]byte(send_msg))	
-		    if err != nil {	
-		        log.Fatal(err)	
-		    }	
-		    //fmt.Printf("Send:******* %s\n", msg[:m])
-		}	
+func EchoHandler(ws *websocket.Conn) {
+	msg := make([]byte, 512)
+	var sd,send_msg string
+	for{
+		n, err := ws.Read(msg)
+		if err != nil ||n==0 {
+			log.Fatal(err)
+			break
+		}
+		sd += string(msg[:n])
+		send_msg += "[" + string(msg[:n]) + "]"
 	}
-	
-}
+	cc,_ := base64.StdEncoding.DecodeString(sd)
+	fmt.Printf("Service---Receive:----------- %v--%v\n", cc,time.Now())
 
+	
+	m, err := ws.Write([]byte(send_msg))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Service---Send:----------- %s--%v\n", msg[:m],time.Now())
+}
 
 func Server() {
-	http.Handle("/echo", websocket.Handler(EchoHandler))
+	go http.Handle("/echo", websocket.Handler(EchoHandler))
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: " + err.Error())
+		panic("ListenAndServe: " + err.Error())
 	}
 }
+
+var origin string
+var url string
+var i int
 
 func Client(iplist []string,locIp string) {
+	fmt.Println(iplist)
 	chanMax := len(iplist)
-	writeChan := make(chan string, chanMax)
+	writeChan := make(chan int, chanMax)
 	for {
-		for _,fsmIp := range iplist{
-			writeChan<-fsmIp
-			go Send(writeChan,locIp)
+		i++
+		time.Sleep(1 * time.Second)
+		if i == 999999999 {
+			i = 0
+			i++
 		}
-		
-		time.Sleep(3 * time.Second)
+		writeChan <- i
+		go Send(writeChan, iplist,locIp)
+
 	}
 
 }
 
-func Send(writeChan chan string,locIp string) {
-	var origin string
-	var url string
-	v := <-writeChan
+func Send(writeChan chan int, iplist []string,locIp string) {
+	for _, v := range iplist {
 		origin = "http://" + strings.TrimSpace(v) + ":80/"
 		url = "ws://" + strings.TrimSpace(v) + ":80/echo"
 		ws, err := websocket.Dial(url, "", origin)
-		if err != nil{
-			fmt.Println(locIp)
-		}else{
+		if err == nil {
+			message := []byte(base64.StdEncoding.EncodeToString([]byte(locIp)))
+			ws.Write(message)
+			fmt.Printf("Client---Send: %s---%v--ip:--%v\n", message,time.Now(),v)
+			<-writeChan
 			var msg = make([]byte, 512)
-			//message := []byte(base64.StdEncoding.EncodeToString([]byte(locIp)))
-			//msint,err1:=ws.Read(msg)
-			// if err1 != nil{
-				fmt.Println(ws,msg,"-----------------",locIp)
-			// }
+			m, _ := ws.Read(msg)
+			fmt.Printf("Client---Receive: %s---%v--ip:--%v\n", msg[:m],time.Now(),v)
 			
 		}
-}
-
-func CheckBeat(receivedata string,now time.Time,ip string){
-	fmt.Printf("%s,%s,%s\n",receivedata,now,ip)
+	}
 }
