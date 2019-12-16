3af8adc3b040addf5fd9cc4a021e48a420b5127f
shanhuijie <2270149665@qq.com>
2019-12-16 15:10:44 +0800

    '2019-12-16-75%'

--- fiveinarow/bg.go
+++ fiveinarow/bg.go
@@ -1,12 +1,12 @@
 package fiveinarow
 
 import(
-	 "fmt"
+	 _"fmt"
 	 "sort"
 )
 const(
 	matrix = 50*50
-	point  =  5
+	point  =  3
 )
 type Coordinat struct{
 	x	int
@@ -36,29 +36,58 @@ func InArray(need Coordinat, needArr []Coordinat) bool {
    return false
 }
 
+func inverted(tmp []int) bool {		//倒序检查
+	var i int
+	for k := len(tmp)-1; k>=0;k--{
+		if k == 0{					//最后一个下标说明无法对比
+			return false
+		}
+		if tmp[k]-1 == tmp[k]{		//说明值是连续数字
+			i++
+			if i == point{			//如果达到连续数就返回
+				return true
+			}
+		}else{
+			return false
+		}
+	}
+	return false
+}
+
+func postive(tmp []int) bool {		//正序检查
+	var i int
+	for ck, cv := range tmp {
+		if ck == len(tmp)-1{		//最后一个下标说明无法对比
+			return false
+		}
+		if cv+1 == tmp[ck+1] {		//说明值是连续数字
+			i++
+			if i == point{			//如果达到连续数就返回
+				return true
+			}
+		}else{
+			return false
+		}
+	}
+	return false
+}
+
 func lengthways(inat *Allinat,coor Coordinat) bool {
 	var (
 		max,min int = coor.x+4,coor.x-4
-		i int
 		tmp []int
 	)
+	if min < 0 {
+		min = 0
+	}
 	for _,c := range inat.key{
-		if (max > c.x && c.y == coor.y) || (min > c.x  && c.y == coor.y){
+		if (max >= c.x && c.y == coor.y) || (min >= c.x  && c.y == coor.y){
 			tmp = append(tmp,c.x)
 		}
 	}
 	sort.Ints(tmp)
-	for ck, cv := range tmp {
-		if ck == len(tmp)-1{
-			return false
-		}
-		if cv+1 == tmp[ck+1] {
-			i++
-		}
-		if i == point{
-			fmt.Println("X :",coor)
-			return true
-		}
+	if (inverted(tmp) == true) || (postive(tmp) == true) {
+		return true
 	}
 	return false
 }
@@ -66,26 +95,16 @@ func lengthways(inat *Allinat,coor Coordinat) bool {
 func crosswise(inat *Allinat,coor Coordinat) bool {
 	var (
 		max,min int = coor.y+4,coor.y-4
-		i int
 		tmp []int
 	)
 	for _,c := range inat.key{
-		if (max > c.y && c.x == coor.x) || (min > c.y  && c.x == coor.x){
+		if (max >= c.y && c.x == coor.x) || (min >= c.y  && c.x == coor.x){
 			tmp = append(tmp,c.y)
 		}
 	}
 	sort.Ints(tmp)
-	for ck, cv := range tmp {
-		if ck == len(tmp)-1{
-			return false
-		}
-		if cv+1 == tmp[ck+1] {
-			i++
-		}
-		if i == point{
-			fmt.Println("Y :",coor)
-			return true
-		}
+	if (inverted(tmp) == true) || (postive(tmp) == true) {
+		return true
 	}
 	return false
 }
@@ -94,7 +113,7 @@ func IsFive(inat *Allinat,coor Coordinat) bool {
 	ok := crosswise(inat,coor)
 	ok2 := lengthways(inat,coor)
 	//slope(inat)
-	if ok == true && ok2 == true{
+	if ok == true || ok2 == true{
 		return true
 	}
 	return false
--- fiveinarow/fiveinarow_test.go
+++ fiveinarow/fiveinarow_test.go
@@ -10,17 +10,23 @@ import(
 func TestAddAllinat(t *testing.T){
 	 c := Allinat{}
 	 chess := Coordinat{}
-	//  chess.x = 10
-	//  chess.y = 15
-	//  c.AddCoordinat(chess)
+	 chess.x = 10
+	 chess.y = 15
+	 c.AddCoordinat(chess)
 	 
-	 for i:=0; i<20000; i++{
+	 for i:=0; i<11500; i++{
 		rand.Seed(time.Now().UnixNano())
-		chess.x = rand.Intn(9)
-		chess.y = rand.Intn(9)
+		chess.x = rand.Intn(8)
+		chess.y = rand.Intn(8)
 		c.AddCoordinat(chess)
-		
 	 }
+	// for i:=0; i<=13;i=i+2{
+	// 	for j:=6;j>=0;j--{
+	// 		chess.x = i
+	// 		chess.y = j
+	// 		c.AddCoordinat(chess)
+	// 	}
+	// }
 	 for k,v := range c.key{
 		 fmt.Println(k,v)
 	 }
