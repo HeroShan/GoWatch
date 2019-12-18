4179f25550b6b8d9eae285db5eda192588b724bb
shanhuijie <2270149665@qq.com>
2019-12-18 17:02:07 +0800

    '2019-12-18fixslope51%'

--- fiveinarow/fiveinarow_test.go
+++ fiveinarow/fiveinarow_test.go
@@ -3,22 +3,22 @@ package fiveinarow
 import(
 	"testing"
 	"fmt"
-	// "math/rand"
-	// "time"
+	"math/rand"
+	"time"
 )
 
 func TestAddAllinat(t *testing.T){
 	 c := Allinat{}
 	 chess := Coordinat{}
-	//  for i:=0; i<80; i++{
-	// 	rand.Seed(time.Now().UnixNano())
-	// 	chess.x = rand.Intn(6)
-	// 	chess.y = rand.Intn(6)
-	// 	c.AddCoordinat(chess)
-	//  }
-	 chess.x = 10
-	 chess.y = 7
-	 c.AddCoordinat(chess)
+	 for i:=0; i<80; i++{
+		rand.Seed(time.Now().UnixNano())
+		chess.x = rand.Intn(6)
+		chess.y = rand.Intn(6)
+		c.AddCoordinat(chess)
+	 }
+	//  chess.x = 10
+	//  chess.y = 7
+	//  c.AddCoordinat(chess)
 	// for i:=3; i<=13;i++{
 	// 	for j:=6;j>=0;j--{
 	// 		chess.x = i
