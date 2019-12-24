a97cf01f4fc2e3ab2117a04ec7779a707dddafcf
shanhuijie <2270149665@qq.com>
2019-12-24 14:17:04 +0800

    'tfX'

--- tf.go
+++ tf.go
@@ -6,30 +6,33 @@ import(
 	"GoWatch/fiveinarow"
 )
 func main(){
-	c := fiveinarow.Allinat{}
-	 chess := fiveinarow.Coordinat{}
-	 for i:=0; i<10000; i++{
-		if i % 2 == 0{
-			chess.Color = "white"
-		}else{
-			chess.Color = "black"
-		}
-		rand.Seed(time.Now().UnixNano())
-		chess.X = rand.Intn(10)
-		chess.Y = rand.Intn(10)
-		c.AddCoordinat(chess)
-	 }
-	//  chess.X = 10
-	//  chess.Y = 7
-	//  c.AddCoordinat(chess)
-	// for i:=3; i<=13;i++{
-	// 	for j:=6;j>=0;j--{
-	// 		chess.X = i
-	// 		chess.Y = j
-	// 		c.AddCoordinat(chess)
-	// 	}
-	// }
-	 for k,v := range c.Key{
-		 fmt.Println(k,v)
-	 }
+	c := Allinat{}
+	chess := Coordinat{}
+	for i:=0; i<88888; i++{
+	   if i % 2 == 0{
+		   chess.Color = "white"
+	   }else{
+		   chess.Color = "black"
+	   }
+	   rand.Seed(time.Now().UnixNano())
+	   chess.X = rand.Intn(8)
+	   chess.Y = rand.Intn(9)
+	   x,xarr := c.AddCoordinat(chess)
+	   if x == true {
+		   fmt.Println("true",xarr)
+	   }
+	}
+   //  chess.X = 10
+   //  chess.Y = 7
+   //  c.AddCoordinat(chess)
+   // for i:=3; i<=13;i++{
+   // 	for j:=6;j>=0;j--{
+   // 		chess.X = i
+   // 		chess.Y = j
+   // 		c.AddCoordinat(chess)
+   // 	}
+   // }
+	for k,v := range c.Key{
+		fmt.Println(k,v)
+	}
 }
