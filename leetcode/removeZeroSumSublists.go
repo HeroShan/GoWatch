511cdb95a58cc36d17b8f0f63d076fc86da9b102
shanhuijie <2270149665@qq.com>
2020-05-14 16:11:35 +0800

    'Merge'

--- leetcode/removeZeroSumSublists.go
+++ leetcode/removeZeroSumSublists.go
@@ -32,3 +32,25 @@ func RemoveZeroSumSublists(head *ListNode) *ListNode {
 	}
 	return p.Next
 }
+
+//https://leetcode-cn.com/problems/sorted-merge-lcci/
+func Merge(A []int, m int, B []int, n int)  {
+	AIndex :=m-1
+	BIndex := n-1
+	idx := len(A)-1
+	for AIndex>=0&&BIndex>=0{
+		   if A[AIndex]>B[BIndex]{
+				 A[idx] = A[AIndex]
+			AIndex--
+		}else {
+			 A[idx] = B[BIndex]
+			BIndex--
+		}
+		idx--
+	}
+	for BIndex>=0{
+		A[idx] = B[BIndex]
+		BIndex--
+		idx--
+	}
+}
