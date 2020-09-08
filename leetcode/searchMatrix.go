7537368202afdaa6a7b93d99c8e14b1abe213979
shanhuijie <shanhuijie@woncloud-sec.com>
2020-09-08 18:17:23 +0800

    twoSum()

--- leetcode/searchMatrix.go
+++ leetcode/searchMatrix.go
@@ -28,3 +28,22 @@ func SearchMatrix(matrix [][]int, target int) bool {
 	return false
 
 }
+
+func twoSum(nums []int, target int) []int {
+    var result []int
+    var kMax,cursor int
+    kMax = len(nums)
+    for k,v := range nums{
+        if k == kMax{
+            break
+        }
+        for cursor = k+1;cursor < kMax;cursor++{
+            if v+nums[cursor] == target{
+            result = append(result,k)
+            result = append(result,cursor)
+            }
+        }
+        
+    }
+    return result
+}
