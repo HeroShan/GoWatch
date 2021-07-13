a503ced1d95b780d594ea055e32ac8203ec2e600
Frisk <2270149665@qq.com>
2021-07-13 17:08:18 +0800

    btree truncate

--- leetcode/bTree.go
+++ leetcode/bTree.go
@@ -15,13 +15,20 @@ type bNode struct {
 func NewBtree() *BTree {
 	return new(BTree)
 }
+func (bt *BTree) Truncate() {
+	*bt = BTree{nil, nil}
+	return
+}
 
+//遍历btree  所有元素
 func (bt *BTree) EachBt() {
 	for bk, bn := range bt.treeList {
 		i := 0
 		bn.EachBn(bk, i)
 	}
 }
+
+//遍历每个枝上的节点
 func (bn *bNode) EachBn(bk int, nk int) {
 	if bn.next == nil {
 		return
@@ -32,15 +39,37 @@ func (bn *bNode) EachBn(bk int, nk int) {
 	bn.EachBn(bk, nk)
 }
 
-func (bt *BTree) AddBtree(head interface{}, item interface{}) {
+//根据头进行追加btree 元素
+//头为空  追加到根
+//头不为空  找到头追加到下一个节点   next顺移到下一个节点
+func (bt *BTree) AddBtree(head interface{}, item interface{}) bool {
+	var nilHead, next bNode
 	if head == nil {
-		var nilHead, next bNode
 		nilHead.data = item
 		nilHead.next = &next
 		bt.treeList = append(bt.treeList, &nilHead)
-		return
+		return true
 	} else {
+		for _, v := range bt.treeList {
+			for v.next != nil {
+				if v.data == head {
+					if v.next == nil {
+						nilHead.data = item
+						nilHead.next = &next
+						v.next = &nilHead
+						return true
+					} else {
+						next = *v.next
+						nilHead.data = item
+						nilHead.next = &next
+						v.next = &nilHead
+						return true
+					}
+				}
+				v = v.next
+			}
 
+		}
 	}
-
+	return false
 }
--- leetcode/bTree_test.go
+++ leetcode/bTree_test.go
@@ -5,17 +5,72 @@ import (
 	"testing"
 )
 
+func TestBTree_Truncate(t *testing.T) {
+	var r bool
+	bt := new(BTree)
+	r = bt.AddBtree(nil, 0)
+	fmt.Println(r)
+	for i := 0; i < 2; i++ {
+		p := i + 1
+		r = bt.AddBtree(i, p)
+		fmt.Println(r, i, p)
+	}
+
+	bt.AddBtree(nil, 11)
+	for j := 11; j < 30; j++ {
+		bt.AddBtree(j, j+1)
+	}
+	bt.EachBt()
+	bt.Truncate()
+	fmt.Println("truncate!")
+	bt.EachBt()
+}
+
+func BenchmarkBNode_EachBn(b *testing.B) {
+	var r bool
+	bt := new(BTree)
+	r = bt.AddBtree(nil, 0)
+	fmt.Println(r)
+	for i := 0; i < 2; i++ {
+		p := i + 1
+		r = bt.AddBtree(i, p)
+		fmt.Println(r, i, p)
+	}
+
+	bt.AddBtree(nil, 11)
+	for j := 11; j < 30; j++ {
+		bt.AddBtree(j, j+1)
+	}
+	bt.EachBt()
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		bt.EachBt()
+	}
+}
+
 func TestBNode_EachBn(t *testing.T) {
 	bt := NewBtree()
 	bt.AddBtree(nil, "1")
-	bt.AddBtree(nil, "2")
+	bt.AddBtree(nil, "5")
 	bt.EachBt()
 }
 
 func TestBTree_AddBtree(t *testing.T) {
+	var r bool
 	bt := NewBtree()
-	bt.AddBtree(nil, "1")
-	fmt.Printf("%#v\n", bt.treeList[0])
+	r = bt.AddBtree(nil, 0)
+	fmt.Println(r)
+	for i := 0; i < 2; i++ {
+		p := i + 1
+		r = bt.AddBtree(i, p)
+		fmt.Println(r, i, p)
+	}
+
+	bt.AddBtree(nil, 11)
+	for j := 11; j < 30; j++ {
+		bt.AddBtree(j, j+1)
+	}
+	bt.EachBt()
+
 }
 
 func TestNewBtree(t *testing.T) {
